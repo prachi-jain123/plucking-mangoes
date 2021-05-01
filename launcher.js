2c88957ca2cc6f9bf2c0dfdabda69272cf960883
prachi-jain123 <77056757+prachi-jain123@users.noreply.github.com>
2021-05-01 16:42:20 +0400

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,28 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+           pointB:pointB,
+            stiffness: 0.004,
+            length: 1
+        }
+        this.bodyA = bodyB;
+        this.pointB = pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    fly(){
+        this.chain.bodyA=null
+        }
+
+    display(){
+        if (this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
+    }
+}
